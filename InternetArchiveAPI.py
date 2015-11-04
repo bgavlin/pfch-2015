ee7bfffa9785945645777e232e86e50eb62194d0
bgavlin <bgavlin@gmail.com>
2015-11-04 09:49:43 -0500

    Change to JSON Dump
    Need to find a way to change CSV delimiter in order to make csv writer
    work properly

--- InternetArchiveAPI.py
+++ InternetArchiveAPI.py
@@ -3,16 +3,16 @@ import csv
 
 #r = requests.get ("https://archive.org/advancedsearch.php?q=old+time+radio&fl[]=creator,coverage,date,description,title,type,subject,identifier&rows=50&page=1&output=json")   
 
-r = requests.get ("https://archive.org/advancedsearch.php?q=creator:(Old+Time+Radio+Researchers)+AND+mediatype:(audio)&fl[]&rows=50&page=1&output=json") 
+#r = requests.get ("https://archive.org/advancedsearch.php?q=creator:(Old+Time+Radio+Researchers)+AND+mediatype:(audio)&fl[]&rows=50&page=1&output=json") 
+
+r = requests.get ("https://archive.org/advancedsearch.php?q=creator:(Old+Time+Radio+Researchers)+AND+mediatype:(audio)&fl[]&rows=200&output=json") 
 
 response_value = json.loads(r.text)
 
 #print(response_value['response']['docs'])
 #print(response_value['response']['docs'][0]['title'])
 
-#with open('RadioShows.txt','w') as outfile:
-with open('IA_Radio_Shows.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+with open('RadioShows.txt','w') as outfile:
     metadata={}
     
     for a_doc in response_value['response']['docs']:
@@ -21,14 +21,19 @@ with open('IA_Radio_Shows.csv', 'w', newline='') as csvfile:
             metadata['description'] = a_doc['description']
         except:
             continue
-        try:
-            title = metadata['title']
-        except:
-            title = False
-        try:
-            description = metadata['description']
-        except:
-            description = False
-        #print (metadata)
-        #json.dump(metadata, outfile, sort_keys = True, indent = 4, ensure_ascii=False)
-        writer.writerow([title,description])
+        #print (description)
+        json.dump(metadata, outfile, indent = 4, ensure_ascii=False)  
+
+        
+        
+#with open('IA_Radio_Shows2.csv', 'w', newline='') as csvfile:   
+#    writer = csv.writer(csvfile, delimiter = " ")
+#        try:
+#            title = metadata['title']
+#        except:
+#            title = False
+#        try:
+#            description = metadata['description']
+#        except:
+#            description = False
+#        writer.writerow([title,description])
