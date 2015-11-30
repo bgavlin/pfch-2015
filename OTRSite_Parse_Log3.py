09d12e4b0cda57adbab0132c09ecc1dc8a46dc80
bgavlin <bgavlin@gmail.com>
2015-11-29 23:04:20 -0500

    Fixed script

--- OTRSite_Parse_Log3.py
+++ OTRSite_Parse_Log3.py
@@ -3,56 +3,55 @@ import csv
 
 with open("OTRSiteScrape.txt","r") as data:
     
-#    for row in data.readlines():
     text=data.read()
-    
-#    series_title = re.compile('Series\:\s\"(.+?)\"', re.IGNORECASE)
-#    all_titles = series_title.finditer(text)
-#    announcer_name = re.compile(r'Announcer\:\s(.+?\s.+?)[\s\\]',re.IGNORECASE)
-#    all_announcers = announcer_name.findall(text)
-#    show_stars = re.compile(r'stars:\s(.+?\s.+?)\\',re.IGNORECASE)
-#    all_stars = show_stars.findall(text)
-    
-    search_string = re.compile(r'Series\:\s\"(.+?)\".*?Announcer\:\s(.+?\s.+?)[\s\\].*?stars:\s(.+?\s.+?)\\', re.IGNORECASE)
 
-
+    search_string = re.compile(r'\[[^\[\]]+Series\:\s\"([\w\s]+)[^\[\]]+\]\[[^\[\]][\w\s="<>\\:./,\'-]+stars\:\s([^\[\]]+?)\\', re.IGNORECASE)
+    
+    host_search = re.compile(r'\[[^\[\]]+Series\:\s\"([\w\s]+)[^\[\]]+\]\[[^\[\]]*?host\:\s([^\[\]]+?)\\', re.IGNORECASE)
+    
+    announcer_search = re.compile(r'\[[^\[\]]+Series\:\s\"([\w\s]+)[^\[\]]+\]\[[^\[\]]*?announcer\:\s([^\[\]]+?)\\', re.IGNORECASE)
     
     with open('Shows_Parsed.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
              
         for match in search_string.finditer(text):    
             try:
                 title=match.group(1)
             except:
                 title= False       
-            try: 
-                announcer=match.group(2)
-            except:
-                announcer=False
             try:
-                stars=match.group(3)
+                stars=match.group(2)
             except:
                 stars=False
             
-        writer.writerow([title,announcer,stars])
+            writer.writerow([title,stars])
                 
-
-#         for match in series_title.finditer(text):    
-#            try:
-#                title=match.group(1)
-#            except:
-#                title= "False"
-#        
-#        for match in announcer_name.finditer(text):    
-#            try:
-#                announcer=match.group(1)
-#            except:
-#                announcer= "False"
-#                
-#        for match in show_stars.finditer(text):    
-#            try:
-#                stars=match.group(1)
-#            except:
-#                stars= "False"              
+    with open('Hosts_Parsed.csv','w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        
+        for match in host_search.finditer(text):
+            try:
+                show=match.group(1)
+            except:
+                show=False
+            try:
+                host=match.group(2)
+            except:
+                host=False
                 
+            writer.writerow([show, host])
             
+    with open('Announcers_Parsed.csv','w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        
+        for match in announcer_search.finditer(text):
+            try:
+                show_title=match.group(1)
+            except:
+                show_title=False
+            try:
+                announcer=match.group(2)
+            except:
+                announcer=False
+                    
+            writer.writerow([show_title, announcer])
